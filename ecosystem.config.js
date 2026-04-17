da7d73cdc480b5594610e47e38a9b33ee811f775
Kévin Dupas <38907117+kevindupas@users.noreply.github.com>
2026-04-17 23:35:49 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,12 @@
 module.exports = {
   apps: [{
     name: 'udp-echo',
-    script: 'authbind',
-    args: '--deep node udp-server.js',
+    script: './udp-server.js',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '128M',
-    env: { UDP_PORT: 5005, TCP_PORT: 443 },
+    env: { UDP_PORT: 5005, TCP_PORT: 8230 },
     error_file: '/var/log/udp-echo/error.log',
     out_file: '/var/log/udp-echo/out.log',
     log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
